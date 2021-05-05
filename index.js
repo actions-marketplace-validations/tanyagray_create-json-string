e5975fb14cb7156e359ef877df7a1e2969870fbb
Tanya Gray <153077+tanyagray@users.noreply.github.com>
2021-05-05 21:59:49 +1200

    Update index.js

--- index.js
+++ index.js
@@ -1,7 +1,6 @@
 const core = require('@actions/core');
 const fs = require('fs');
 
-const fileName = core.getInput('file-name');
 const inputPrefix = "INPUT_"; // This is the prefix used by GitHub Actions
 const path = require("path");
 const fullPath = path.join(process.env.GITHUB_WORKSPACE, fileName);
@@ -9,18 +8,12 @@ const fullPath = path.join(process.env.GITHUB_WORKSPACE, fileName);
 var obj = {};
 
 Object.keys(process.env).forEach(function(key) {
-  if(key.startsWith(inputPrefix) && key != "INPUT_FILE-NAME") {
+  if(key.startsWith(inputPrefix)) {
     obj[key.substring(inputPrefix.length)] = process.env[key];
   }
 });
   
 const fileContent = JSON.stringify(obj);
-
-fs.writeFile(fullPath, fileContent, function (error) {
-  if (error) {
-    core.setFailed(error.message);
-  }
-  
-  console.log(`Successfully written file ${fullPath} with content ${fileContent}`);
-  core.setOutput("full-path", fullPath);
-});
+ 
+console.log(`Successfully created json with content ${fileContent}`);
+core.setOutput("json_string", fileContent);
